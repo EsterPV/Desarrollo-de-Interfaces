9e1ca80e34f5d1bdbbe45aa8e6415a50517994f2
Ester <esterperez51@gmail.com>
2021-11-19 17:45:28 +0100

    Ejercicio05
    Manejo de BBDD desde un fichero txt

---
+++ Visual_Basic.NET/Ejercicio05_PerezEster/Ejercicio05_PerezEster/Form1.Designer.vb
@@ -0,0 +1,354 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.txtDni = New System.Windows.Forms.TextBox()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.dgvContactos = New System.Windows.Forms.DataGridView()
+        Me.NOMBRE = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DNI = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TELEFONO = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DIRECCION = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.SEXO = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cboSexo = New System.Windows.Forms.ComboBox()
+        Me.ErrorProviderValidar = New System.Windows.Forms.ErrorProvider(Me.components)
+        CType(Me.dgvContactos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.ErrorProviderValidar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Magneto", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Indigo
+        Me.Label1.Location = New System.Drawing.Point(36, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(198, 28)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CONTACTOS"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Lucida Sans Unicode", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label2.Location = New System.Drawing.Point(36, 74)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Lucida Sans Unicode", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label3.Location = New System.Drawing.Point(36, 103)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "DNI"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Lucida Sans Unicode", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label4.Location = New System.Drawing.Point(36, 137)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Teléfono"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Lucida Sans Unicode", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label5.Location = New System.Drawing.Point(36, 172)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(81, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Dirección"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Lucida Sans Unicode", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label6.Location = New System.Drawing.Point(36, 203)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(46, 17)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Sexo"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(151, 74)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(272, 22)
+        Me.txtNombre.TabIndex = 6
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(151, 167)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(272, 22)
+        Me.txtDireccion.TabIndex = 8
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(151, 134)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(272, 22)
+        Me.txtTelefono.TabIndex = 9
+        '
+        'txtDni
+        '
+        Me.txtDni.Location = New System.Drawing.Point(151, 102)
+        Me.txtDni.Name = "txtDni"
+        Me.txtDni.Size = New System.Drawing.Size(272, 22)
+        Me.txtDni.TabIndex = 10
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.btnAgregar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAgregar.Font = New System.Drawing.Font("Lucida Sans Typewriter", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAgregar.Location = New System.Drawing.Point(583, 67)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(160, 29)
+        Me.btnAgregar.TabIndex = 11
+        Me.btnAgregar.Text = "AGREGAR"
+        Me.btnAgregar.UseVisualStyleBackColor = False
+        '
+        'btnModificar
+        '
+        Me.btnModificar.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.btnModificar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnModificar.Font = New System.Drawing.Font("Lucida Sans Typewriter", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnModificar.Location = New System.Drawing.Point(583, 99)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(160, 29)
+        Me.btnModificar.TabIndex = 12
+        Me.btnModificar.Text = "MODIFICAR"
+        Me.btnModificar.UseVisualStyleBackColor = False
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.BackColor = System.Drawing.Color.LightCoral
+        Me.btnEliminar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnEliminar.Font = New System.Drawing.Font("Lucida Sans Typewriter", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEliminar.Location = New System.Drawing.Point(583, 131)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(160, 29)
+        Me.btnEliminar.TabIndex = 13
+        Me.btnEliminar.Text = "ELIMINAR"
+        Me.btnEliminar.UseVisualStyleBackColor = False
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.btnGuardar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnGuardar.Font = New System.Drawing.Font("Lucida Sans Typewriter", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGuardar.Location = New System.Drawing.Point(583, 166)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(160, 29)
+        Me.btnGuardar.TabIndex = 14
+        Me.btnGuardar.Text = "GUARDAR"
+        Me.btnGuardar.UseVisualStyleBackColor = False
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.Color.LightCoral
+        Me.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSalir.Font = New System.Drawing.Font("Lucida Sans Typewriter", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.Location = New System.Drawing.Point(583, 200)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(160, 29)
+        Me.btnSalir.TabIndex = 15
+        Me.btnSalir.Text = "SALIR"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'dgvContactos
+        '
+        Me.dgvContactos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvContactos.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.NOMBRE, Me.DNI, Me.TELEFONO, Me.DIRECCION, Me.SEXO})
+        Me.dgvContactos.Location = New System.Drawing.Point(11, 21)
+        Me.dgvContactos.Name = "dgvContactos"
+        Me.dgvContactos.RowHeadersWidth = 51
+        Me.dgvContactos.RowTemplate.Height = 24
+        Me.dgvContactos.Size = New System.Drawing.Size(704, 179)
+        Me.dgvContactos.TabIndex = 16
+        '
+        'NOMBRE
+        '
+        Me.NOMBRE.HeaderText = "NOMBRE"
+        Me.NOMBRE.MinimumWidth = 6
+        Me.NOMBRE.Name = "NOMBRE"
+        Me.NOMBRE.Width = 125
+        '
+        'DNI
+        '
+        Me.DNI.HeaderText = "DNI"
+        Me.DNI.MinimumWidth = 6
+        Me.DNI.Name = "DNI"
+        Me.DNI.Width = 125
+        '
+        'TELEFONO
+        '
+        Me.TELEFONO.HeaderText = "TELÉFONO"
+        Me.TELEFONO.MinimumWidth = 6
+        Me.TELEFONO.Name = "TELEFONO"
+        Me.TELEFONO.Width = 125
+        '
+        'DIRECCION
+        '
+        Me.DIRECCION.HeaderText = "DIRECCIÓN"
+        Me.DIRECCION.MinimumWidth = 6
+        Me.DIRECCION.Name = "DIRECCION"
+        Me.DIRECCION.Width = 125
+        '
+        'SEXO
+        '
+        Me.SEXO.HeaderText = "SEXO"
+        Me.SEXO.MinimumWidth = 6
+        Me.SEXO.Name = "SEXO"
+        Me.SEXO.Width = 125
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.dgvContactos)
+        Me.GroupBox1.Font = New System.Drawing.Font("Lucida Sans Typewriter", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(39, 263)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(721, 213)
+        Me.GroupBox1.TabIndex = 17
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "LISTA DE CONTACTOS"
+        '
+        'cboSexo
+        '
+        Me.cboSexo.FormattingEnabled = True
+        Me.cboSexo.Items.AddRange(New Object() {"Hombre", "Mujer", "Otro"})
+        Me.cboSexo.Location = New System.Drawing.Point(151, 200)
+        Me.cboSexo.Name = "cboSexo"
+        Me.cboSexo.Size = New System.Drawing.Size(272, 24)
+        Me.cboSexo.TabIndex = 18
+        '
+        'ErrorProviderValidar
+        '
+        Me.ErrorProviderValidar.ContainerControl = Me
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.BackgroundImage = Global.Ejercicio05_PerezEster.My.Resources.Resources.unnamed__1_
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 488)
+        Me.Controls.Add(Me.cboSexo)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnModificar)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.txtDni)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.txtDireccion)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Inicio"
+        CType(Me.dgvContactos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.ErrorProviderValidar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtDireccion As TextBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents txtDni As TextBox
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents dgvContactos As DataGridView
+    Friend WithEvents NOMBRE As DataGridViewTextBoxColumn
+    Friend WithEvents DNI As DataGridViewTextBoxColumn
+    Friend WithEvents TELEFONO As DataGridViewTextBoxColumn
+    Friend WithEvents DIRECCION As DataGridViewTextBoxColumn
+    Friend WithEvents SEXO As DataGridViewTextBoxColumn
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents cboSexo As ComboBox
+    Friend WithEvents ErrorProviderValidar As ErrorProvider
+End Class
---
+++ Visual_Basic.NET/Ejercicio05_PerezEster/Ejercicio05_PerezEster/Form1.vb
@@ -0,0 +1,182 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Dim validar, validarDni As Boolean
+    Dim filaActual As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim archivoLeer As StreamReader
+        archivoLeer = New StreamReader(".\datos.txt")
+
+        While Not archivoLeer.EndOfStream
+            Dim cadena As String = archivoLeer.ReadLine
+            Dim leer As String() = cadena.Split(New Char() {";"})
+            dgvContactos.Rows.Add(leer)
+        End While
+        archivoLeer.Close()
+    End Sub
+    Function ValidarDatos(parnombre As String, pardni As String, partelefono As String, pardireccion As String, parsexo As String) As Boolean
+        If parnombre = "" Then
+            MsgBox("Ingrese el nombre", MsgBoxStyle.Critical, "Información")
+            ErrorProviderValidar.SetError(txtNombre, "Ingrese nombre")
+            validar = False
+        ElseIf pardni = "" Then
+            ErrorProviderValidar.SetError(txtDni, "Ingrese DNI")
+            MsgBox("Ingrese el dni", MsgBoxStyle.Critical, "Información")
+            validar = False
+        ElseIf partelefono = "" Then
+            MsgBox("Ingrese el telefono", MsgBoxStyle.Critical, "Información")
+            ErrorProviderValidar.SetError(txtTelefono, "Ingrese telefono")
+            validar = False
+        ElseIf pardireccion = "" Then
+            MsgBox("Ingrese la direccion", MsgBoxStyle.Critical, "Información")
+            ErrorProviderValidar.SetError(txtDireccion, "Ingrese direccion")
+            validar = False
+        ElseIf parsexo = "" Then
+            MsgBox("Seleccione el sexo", MsgBoxStyle.Critical, "Información")
+            ErrorProviderValidar.SetError(cboSexo, "Seleccione el sexo")
+            validar = False
+        Else
+            validar = True
+            ErrorProviderValidar.SetError(txtNombre, "")
+            ErrorProviderValidar.SetError(txtDni, "")
+            ErrorProviderValidar.SetError(txtTelefono, "")
+            ErrorProviderValidar.SetError(txtDireccion, "")
+            ErrorProviderValidar.SetError(cboSexo, "")
+        End If
+        Return validar
+    End Function
+
+    Function ValidarIdentificacion(pardni As String) As Boolean
+
+        validarDni = True
+        For i = 0 To dgvContactos.RowCount - 1
+            If pardni = dgvContactos.Rows(i).Cells("DNI").Value Then
+                MsgBox("Ya existe un contacto con ese dni", MsgBoxStyle.Critical, "Información")
+                ErrorProviderValidar.SetError(txtDni, "Ingrese otro DNI")
+                validarDni = False
+                Exit For
+            Else
+                ErrorProviderValidar.SetError(txtDni, "")
+                validarDni = True
+            End If
+
+        Next
+        Return validarDni
+
+    End Function
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+
+        validar = ValidarDatos(txtNombre.Text, txtDni.Text, txtTelefono.Text, txtDireccion.Text, cboSexo.Text)
+        If validar = True Then
+            validarDni = ValidarIdentificacion(txtDni.Text)
+            If (validarDni = True) Then
+                dgvContactos.Rows.Add(txtNombre.Text, txtDni.Text, txtTelefono.Text, txtDireccion.Text, cboSexo.Text)
+                MsgBox("El contacto se ha registrado")
+                limpiar()
+            End If
+        End If
+
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+
+        filaActual = dgvContactos.CurrentRow.Index
+        If validar = True Then
+            If txtDni.Text <> dgvContactos.Rows(filaActual).Cells("DNI").Value Then
+                validarDni = ValidarIdentificacion(txtDni.Text)
+            End If
+            If validarDni = True Then
+                dgvContactos.Rows(filaActual).Cells("NOMBRE").Value = txtNombre.Text
+                dgvContactos.Rows(filaActual).Cells("DNI").Value = txtDni.Text
+                dgvContactos.Rows(filaActual).Cells("TELEFONO").Value = txtTelefono.Text
+                dgvContactos.Rows(filaActual).Cells("DIRECCION").Value = txtDireccion.Text
+                dgvContactos.Rows(filaActual).Cells("SEXO").Value = cboSexo.Text
+                activarBotones()
+                limpiar()
+            End If
+        End If
+
+    End Sub
+
+    Private Sub dgvContactos_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvContactos.CellDoubleClick
+        filaActual = dgvContactos.CurrentRow.Index
+        txtNombre.Text = dgvContactos.Rows(filaActual).Cells("NOMBRE").Value
+        txtDni.Text = dgvContactos.Rows(filaActual).Cells("DNI").Value
+        txtTelefono.Text = dgvContactos.Rows(filaActual).Cells("TELEFONO").Value
+        txtDireccion.Text = dgvContactos.Rows(filaActual).Cells("DIRECCION").Value
+        cboSexo.Text = dgvContactos.Rows(filaActual).Cells("SEXO").Value
+        DesactivarBotones()
+
+
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Dim pregunta As String
+        pregunta = MsgBox("¿Está segura que desea eliminar este contacto", MsgBoxStyle.YesNo, "Información")
+        If pregunta = vbYes Then
+            filaActual = dgvContactos.CurrentRow.Index
+            dgvContactos.Rows.Remove(dgvContactos.Rows(filaActual))
+            MsgBox("El contacto se ha eliminado")
+            ActivarBotones()
+            limpiar()
+        End If
+
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        GuardarDatos()
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Close()
+    End Sub
+
+    Private Sub GuardarDatos()
+        Dim archivoEscritura As StreamWriter
+        Dim linea As String
+        archivoEscritura = New StreamWriter(".\datos.txt")
+        With dgvContactos
+            For i = 0 To .RowCount - 1
+                linea = .Rows(i).Cells("NOMBRE").Value & ";" &
+                        .Rows(i).Cells("DNI").Value & ";" &
+                        .Rows(i).Cells("TELEFONO").Value & ";" &
+                        .Rows(i).Cells("DIRECCION").Value & ";" &
+                        .Rows(i).Cells("SEXO").Value & ";"
+                archivoEscritura.WriteLine(linea)
+            Next
+            MsgBox("Los datos se han guardado correctamente")
+        End With
+        archivoEscritura.Close()
+    End Sub
+
+
+    Private Sub limpiar()
+        txtNombre.Clear()
+        txtDni.Clear()
+        txtTelefono.Clear()
+        txtDireccion.Clear()
+        cboSexo.Text = ""
+    End Sub
+
+    Private Sub ActivarBotones()
+        btnAgregar.Enabled = True
+        btnGuardar.Enabled = True
+        btnModificar.Enabled = False
+        btnEliminar.Enabled = False
+    End Sub
+
+
+
+    Private Sub DesactivarBotones()
+        btnAgregar.Enabled = False
+        btnGuardar.Enabled = False
+        btnModificar.Enabled = True
+        btnEliminar.Enabled = True
+
+    End Sub
+
+End Class
