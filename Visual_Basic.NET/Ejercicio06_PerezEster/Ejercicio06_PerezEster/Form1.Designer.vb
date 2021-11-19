a4fce0f503b35eec3bfd9811b9d86ba4bf05bc4c
Ester <esterperez51@gmail.com>
2021-11-19 17:49:27 +0100

    Ejercicio06
    Programa que importa y exporta ficheros con listas de películas. Las ya incluidas las relaciona con su carátula.

---
+++ Visual_Basic.NET/Ejercicio06_PerezEster/Ejercicio06_PerezEster/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtAnhadir = New System.Windows.Forms.TextBox()
+        Me.btnExportar = New System.Windows.Forms.Button()
+        Me.btnAnhadir = New System.Windows.Forms.Button()
+        Me.btnImportar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.PictureBoxPortada = New System.Windows.Forms.PictureBox()
+        Me.ListBoxPeliculas = New System.Windows.Forms.ListBox()
+        CType(Me.PictureBoxPortada, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(269, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(230, 33)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Lista Películas"
+        '
+        'txtAnhadir
+        '
+        Me.txtAnhadir.Location = New System.Drawing.Point(45, 73)
+        Me.txtAnhadir.Name = "txtAnhadir"
+        Me.txtAnhadir.Size = New System.Drawing.Size(274, 22)
+        Me.txtAnhadir.TabIndex = 1
+        '
+        'btnExportar
+        '
+        Me.btnExportar.BackColor = System.Drawing.Color.Gold
+        Me.btnExportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExportar.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExportar.Location = New System.Drawing.Point(62, 355)
+        Me.btnExportar.Name = "btnExportar"
+        Me.btnExportar.Size = New System.Drawing.Size(123, 36)
+        Me.btnExportar.TabIndex = 2
+        Me.btnExportar.Text = "Exportar"
+        Me.btnExportar.UseVisualStyleBackColor = False
+        '
+        'btnAnhadir
+        '
+        Me.btnAnhadir.BackColor = System.Drawing.Color.Gold
+        Me.btnAnhadir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAnhadir.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAnhadir.Location = New System.Drawing.Point(365, 66)
+        Me.btnAnhadir.Name = "btnAnhadir"
+        Me.btnAnhadir.Size = New System.Drawing.Size(117, 29)
+        Me.btnAnhadir.TabIndex = 3
+        Me.btnAnhadir.Text = "Añadir"
+        Me.btnAnhadir.UseVisualStyleBackColor = False
+        '
+        'btnImportar
+        '
+        Me.btnImportar.BackColor = System.Drawing.Color.Gold
+        Me.btnImportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnImportar.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnImportar.Location = New System.Drawing.Point(359, 355)
+        Me.btnImportar.Name = "btnImportar"
+        Me.btnImportar.Size = New System.Drawing.Size(123, 36)
+        Me.btnImportar.TabIndex = 4
+        Me.btnImportar.Text = "Importar"
+        Me.btnImportar.UseVisualStyleBackColor = False
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.Color.Coral
+        Me.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.Location = New System.Drawing.Point(622, 355)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(123, 36)
+        Me.btnSalir.TabIndex = 5
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'PictureBoxPortada
+        '
+        Me.PictureBoxPortada.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBoxPortada.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBoxPortada.Location = New System.Drawing.Point(547, 117)
+        Me.PictureBoxPortada.Name = "PictureBoxPortada"
+        Me.PictureBoxPortada.Size = New System.Drawing.Size(198, 200)
+        Me.PictureBoxPortada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBoxPortada.TabIndex = 6
+        Me.PictureBoxPortada.TabStop = False
+        '
+        'ListBoxPeliculas
+        '
+        Me.ListBoxPeliculas.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.ListBoxPeliculas.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBoxPeliculas.ForeColor = System.Drawing.Color.White
+        Me.ListBoxPeliculas.FormattingEnabled = True
+        Me.ListBoxPeliculas.ItemHeight = 20
+        Me.ListBoxPeliculas.Items.AddRange(New Object() {"Trainspotting", "Luca", "Spencer", "Last night in Soho", "Una joven prometedora", "El club de la lucha", "Scott Pilgrim VS the world"})
+        Me.ListBoxPeliculas.Location = New System.Drawing.Point(45, 117)
+        Me.ListBoxPeliculas.Name = "ListBoxPeliculas"
+        Me.ListBoxPeliculas.Size = New System.Drawing.Size(437, 184)
+        Me.ListBoxPeliculas.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gold
+        Me.BackgroundImage = Global.Ejercicio06_PerezEster.My.Resources.Resources.claqueta_cultivo_fondo_negro_23_2147698844_1
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListBoxPeliculas)
+        Me.Controls.Add(Me.PictureBoxPortada)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnImportar)
+        Me.Controls.Add(Me.btnAnhadir)
+        Me.Controls.Add(Me.btnExportar)
+        Me.Controls.Add(Me.txtAnhadir)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Lista Películas"
+        CType(Me.PictureBoxPortada, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtAnhadir As TextBox
+    Friend WithEvents btnExportar As Button
+    Friend WithEvents btnAnhadir As Button
+    Friend WithEvents btnImportar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents PictureBoxPortada As PictureBox
+    Friend WithEvents ListBoxPeliculas As ListBox
+End Class
---
+++ Visual_Basic.NET/Ejercicio06_PerezEster/Ejercicio06_PerezEster/Form1.vb
@@ -0,0 +1,88 @@
+﻿Public Class Form1
+    Private Sub btnAnhadir_Click(sender As Object, e As EventArgs) Handles btnAnhadir.Click
+        ListBoxPeliculas.Items.Add(txtAnhadir.Text)
+        txtAnhadir.Text = ""
+        txtAnhadir.Focus()
+    End Sub
+
+    Private Sub btnExportar_Click(sender As Object, e As EventArgs) Handles btnExportar.Click
+        Dim guardar As New SaveFileDialog
+        guardar.Filter = "Archivo Text | *.txt"
+        If guardar.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim sw As New System.IO.StreamWriter(guardar.FileName)
+
+            For Each item In ListBoxPeliculas.Items
+                sw.WriteLine(item.ToString())
+            Next
+
+            sw.Close()
+        End If
+
+        Dim respuesta As MsgBoxResult
+        respuesta = MessageBox.Show("¿Desea limpiar la lista", "Nota", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If respuesta = vbYes Then
+            ListBoxPeliculas.Items.Clear()
+        End If
+
+    End Sub
+
+    Private Sub btnImportar_Click(sender As Object, e As EventArgs) Handles btnImportar.Click
+        Dim ruta As String = ""
+        Dim abrirFD As New OpenFileDialog()
+        With abrirFD
+            .Title = "Seleccionar archivos"
+            .Filter = "Archivos Text(*.Txt*.txt)|*.Txt;*txt|Todos los archivos(*.*)|*.*"
+            .Multiselect = False
+            .InitialDirectory = My.Computer.FileSystem.SpecialDirectories.Desktop
+            If .ShowDialog = Windows.Forms.DialogResult.OK Then
+                ruta = .FileName
+            End If
+        End With
+        Dim sr As New System.IO.StreamReader(ruta)
+        While Not sr.EndOfStream
+            ListBoxPeliculas.Items.Add(sr.ReadLine())
+        End While
+        sr.Close()
+
+    End Sub
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+    Private Sub txtAnhadir_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtAnhadir.KeyPress
+        If Asc(e.KeyChar) = 13 Then
+            Me.btnAnhadir.PerformClick()
+        End If
+    End Sub
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBoxPeliculas.SelectedIndexChanged
+        Dim I As String
+        I = ListBoxPeliculas.SelectedItem
+        If ListBoxPeliculas.SelectedItem = "Trainspotting" Then
+            PictureBoxPortada.Image = My.Resources.trainspotting_carátula
+        End If
+        If ListBoxPeliculas.SelectedItem = "Luca" Then
+            PictureBoxPortada.Image = My.Resources.Luca_cover
+        End If
+        If ListBoxPeliculas.SelectedItem = "Spencer" Then
+            PictureBoxPortada.Image = My.Resources.Spencer
+        End If
+        If ListBoxPeliculas.SelectedItem = "Last night in Soho" Then
+            PictureBoxPortada.Image = My.Resources.ltima_noche_en_el_Soho_763337056_large
+        End If
+        If ListBoxPeliculas.SelectedItem = "Una joven prometedora" Then
+            PictureBoxPortada.Image = My.Resources.Una_joven_prometedora_192277202_large
+        End If
+        If ListBoxPeliculas.SelectedItem = "El club de la lucha" Then
+            PictureBoxPortada.Image = My.Resources.El_club_de_la_lucha_996839610_large
+
+        End If
+        If ListBoxPeliculas.SelectedItem = "Scott Pilgrim VS the world" Then
+            PictureBoxPortada.Image = My.Resources.ScottPilgrim
+        End If
+        If ListBoxPeliculas.SelectedItem = "Hereditary" Then
+            PictureBoxPortada.Image = My.Resources.Hereditary_411242403_large
+        End If
+    End Sub
+End Class
+
+
+
